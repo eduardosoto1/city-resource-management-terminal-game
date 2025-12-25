45e52bfc7eef07b8b850c34d7916518853918507
eduardosoto1 <eduardo.soto15006@gmail.com>
2025-12-25 15:00:51 -0800

    Began working on shop menu logic

--- main.py
+++ main.py
@@ -5,6 +5,14 @@
 # Created by: XX
 # Date: 12/13/2025 - 
 
+# How does turn based mechanism work?:
+# - Player decides action (build, buy, assign)
+# - Calculate Capacity
+# - Adjust the Rating
+# - Adjust the Population based on rating
+# - Consume resources based on population size
+# - Check if the player win or loses
+
 import math
 
 WIN_POPULATION = 5000   # Amount of population needed to win
@@ -41,14 +49,16 @@ shop_inv = {
     "wood": ["Wood", 300, "Builds structure", 1],
     "metals": ["Metals", 300, "Power grid & infrastructure (FOR LATER)", 1],
     "ore": ["Ore", 300, "Sell for money or (INVEST | FOR FUTURE)", 1],
-    "animal": ["Animals", 300, "Converts into food & gives water as well", 100],
+    "animals": ["Animals", 300, "Converts into food & gives water as well", 100],
     # Money is the only one that difers from cost as you need ore to buy this one (which is why cost is 1)
     "money": ["Money", 1, "Upkeeps workers, structures, and used for buying", 100],
 }
 
 structures = {
-    "Farm": 1
+    "farm": "Workers assigned here",
+    "house": "Used to fill up population",
 }
+
 # Used to adjust the city's populating and rating
 def adjust_population(city, workers, capacity, rating_bonus, growth_rate):
     try:
@@ -109,25 +119,6 @@ def process_turn(city, worker):
     else:
         return True
 
-# This will display the shop menu
-def print_shop(shop_inv):
-    for key, value in shop_inv.items():
-        name, price, description, growth = value
-        print(f"Name: {name:<10} ${price:<10} Description:{description:<10} +{growth:<10}")
-
-# This will Check if player can afford item, and apply the effects of item.
-def shop_menu(shop_inv):
-    # Print the shop menu
-    print_shop(shop_inv)
-    # Enforce one purchase per turn
-    turn_used = 0   # If true, player should not be able to purchase anything
-    while (turn_used == 0):
-        choice = input("Pick option by name:\n> ").lower().strip()
-        if choice == city[choice]:
-            print("Choice allowed")
-        else:
-            print("Not accepted")
-
 #This assigns workers to structure
 def assign_worker(city, workers, structures):
     # If player has no workers, return
@@ -135,6 +126,49 @@ def assign_worker(city, workers, structures):
         return
     for structure in structures:
         print(structure)
+def main():
+    # Main Game Loop
+    main_game(city, workers)
+
+# The Main Game Loop
+def main_game(city, workers):
+    game_on = True
+    while game_on:
+        # Display city stats
+        display_stats(city)
+
+        # Output to player options they can use
+        for act in actions:
+            print(f"{act}")
+
+        # Ask player to pick option
+        try:
+            choice = input("Pick Option by number: ").strip()
+            choice = int(choice)
+        except ValueError:
+            print("Please enter options 1 between 5.")
+        # Correlate player option with choice
+        match choice:
+            case 1:
+                shop_menu(city, shop_inv)
+            case 2:
+                assign_worker(city, workers, structures)
+            case 3: 
+                pass
+            case 4:
+                game_on = process_turn(city, workers)
+            case 5:
+                game_on = False
+            case _:
+                print("This option is not a choice, choices are 1-5")
+
+# This will change the city's rating
+def adjust_rating():
+    ...
+
+# This will change the city's population
+def adjust_population():
+    ...
 
 # This will display the city's information
 def display_stats(city):
@@ -146,46 +180,36 @@ def display_stats(city):
         print(f"{key}: {value}")
     print()
 
-def main_game(city, workers):
-    # Game Loop ends when false
-    game_on = True
-    while game_on:
-        # Display city stats
-        display_stats(city)
 
-        # Output to player options they can do
-        for action in actions:
-            print(f"{action}")
+# Prints out the shop
+def print_shop(shop_inv):
+    for key, value in shop_inv.items():
+        name, price, description, growth = value
+        print(f"Name: {name:<10} Price:${price:<10} Info:{description:<10} +{growth:<10}")
 
-        # Ask Player to pick option
+# Updates the shop.
+def shop_menu(city, shop_inv):
+    turn_used = 0
+    # Print out each selection
+    print_shop(shop_inv)
+    # Enforce one purchase per turn
+    # If more than 1, player should not purchase anything
+    while turn_used == 0:
+        choice = input("Pick Option by name:\n> ").lower()
+        # If the choice matches the item name, buy it
         try:
-            choice = input("Pick Option (By Number): ").strip()
-            choice = int(choice)
-        except ValueError:
-            print("Please enter option by number.")
-        # Correlate player choice with action
-        match choice:
-            # Buy / Sell Resources | Buy Workers
-            case 1:
-                shop_menu(shop_inv)
-                pass
-            case 2:
-                assign_worker(city, workers, structures)
-                pass
-            case 3:
-                print("Option 3")
-                pass
-            case 4:
-                game_on = process_turn(city, workers)
-                pass
-            case 5:
-                game_on = False
-            case _:
-                print("This option is not a choice. Please enter number 1-5.")
+            choice = get_shop_key(shop_inv, choice)
+            if shop_inv[choice]:
+                turn_used += 1
+        except KeyError:
+            print("Name not found, try again.")
 
-def main():
-    # Main Game Loop
-    main_game(city, workers)
+# Go through each name and 
+def get_shop_key(shop_inv, choice):
+    for key, value in shop_inv.items():
+        search_key = key
+        if key == choice:
+            return search_key
 
 if __name__ == "__main__":
     main()
